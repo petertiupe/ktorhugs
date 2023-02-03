83f5daa0cf4a314c9dbd4e5bac4b0aae599bf06c
peter <peter@tiupe.de>
2023-02-03 16:35:06 +0100

    Client spielereien

--- src/main/kotlin/de/tiupe/ktorclient/KtorClient.kt
+++ src/main/kotlin/de/tiupe/ktorclient/KtorClient.kt
@@ -1,17 +1,25 @@
 package de.tiupe.ktorclient
 
 import io.ktor.client.*
-import io.ktor.client.statement.*
 import io.ktor.client.engine.apache.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import kotlinx.coroutines.runBlocking
 
 
 fun main() {
-    runBlocking {
-        val myKtorClient = HttpClient(Apache)
-        val response: HttpResponse = myKtorClient.get("http://www.tiupe.de")
-        println(response.bodyAsText())
-    }
+        runBlocking{
+            callTiupe()
+            println("tiupe called")
+        }
     println("Der Call wurde erfolgreich ausgeführt...")
+}
+
+suspend fun callTiupe(){
+    // Der Aufruf hier erfolgt vollständig snychron, nur weil ich das immer wieder gerne
+    // vergesse.
+    val myKtorClient = HttpClient(Apache)
+    val response: HttpResponse = myKtorClient.get("http://www.tiupe.de")
+    println(response.bodyAsText())
+    println("Status-Code der Antwort ist: ${response.status}")
 }
